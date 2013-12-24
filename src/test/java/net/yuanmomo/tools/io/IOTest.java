70aa93a7b81677a53d9b190029e3eab72dc71817
yuanmomo <342398690@qq.com>
2013-12-24 16:05:10 +0800

    1. 更新pom 2. 添加测试文件

---
+++ src/test/java/net/yuanmomo/tools/io/IOTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package net.yuanmomo.tools.io;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Administrator
+ *
+ */
+public class IOTest {
+	private static Logger logger = LoggerFactory.getLogger(IOTest.class);
+	@Test
+	public void testWrite(){
+		logger.debug("Start to test IOTest.testWrite()....................");
+		List<String> test=new ArrayList<String>();
+		test.add("yuanmomo1");
+		test.add("yuanmomo2");
+		test.add("yuanmomo3");
+		test.add("yuanmomo4");
+		Write<String> out=new Write<String>();
+		try {
+			out.output("out_put.txt", test);
+		} catch (Exception e) {
+			logger.error("Error",e);
+		}
+		logger.debug("Finish Testing IOTest.testWrite()....................");
+	}
+	@Test
+	public void testRead(){
+		logger.debug("Start to test IOTest.testRead()....................");
+		try {
+			List<String> list = Read.fileToList("out_put.txt");
+			if(list != null){
+				for(String line : list){
+					System.out.println(line);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		logger.debug("Finish Testing IOTest.testRead()....................");
+	}
+}
---
+++ src/test/java/net/yuanmomo/tools/util/properties/PropertiesUtilTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package net.yuanmomo.tools.util.properties;
+
+import java.net.URISyntaxException;
+
+import net.yuanmomo.tools.db.ConnectionTest;
+import net.yuanmomo.tools.util.properties.bean.Bean;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Administrator
+ *
+ */
+public class PropertiesUtilTest {
+	private static Logger logger = LoggerFactory.getLogger(ConnectionTest.class);
+	
+	@Test
+	public void testPropertiesToBean(){
+		logger.debug("Start to test ConnectionTest.testFetchBoneCPConnection()....................");
+		try {
+			logger.info(PropertiesUtil.propertiesToBean(Bean.class, "E:\\Document\\GitRepository\\Tools\\src\\test\\resources\\net\\yuanmomo\\tools\\util\\properties\\bean.properties").toString());
+			logger.info(PropertiesUtil.propertiesToBean(Bean.class, this.getClass().getClassLoader().getResource("net/yuanmomo/tools/util/properties/bean.properties").toURI()).toString());
+		} catch (URISyntaxException e) {
+			logger.error("Error",e);
+		} catch (Exception e) {
+			logger.error("Error",e);
+		}
+		logger.debug("Finish Testing ConnectionTest.testFetchBoneCPConnection()....................");
+	}
+}
